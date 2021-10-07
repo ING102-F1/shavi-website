d3f1f2158b812a1875ac47fa4a58499c5a4d4eac
Rana Kelani <602821@stud.hvl.no>
2021-10-07 23:02:18 +0200

    Generate stations' markers from file

---
+++ js/stations.js
@@ -0,0 +1,44 @@
+var stations = [
+    {
+        city: "Oslo",
+        name:"Aker Brygge",
+        coords: [59.90986575304959, 10.726450606945129],
+        vehicles:{
+            cars: [{model: "TESLA Model 3 (TESLA-1)", quantity: 5}],
+            boats: [{model: "Hydrolift X-22 (HYDRO-1) med Evoy Galeforce Outboard (EVOY-1)", quantity:3}]
+        },
+        description: "",
+        chargers: {
+            car: 6,
+            boat: 4
+        }
+    },
+    {
+        city: "Bergen",
+        name:"Marineholmen",
+        coords: [60.38154003563324, 5.329989669622447],
+        vehicles:{
+            cars: [{model: "TESLA Model 3 (TESLA-1)", quantity: 5}],
+            boats: [{model: "Hydrolift X-22 (HYDRO-1) med Evoy Galeforce Outboard (EVOY-1)", quantity:3}]
+        },
+        description: "",
+        chargers: {
+            car: 6,
+            boat: 4
+        }
+    },
+    {
+        city: "Førde",
+        name:"Førde Båtklubb",
+        coords: [61.45840280666341, 5.845483798503867],
+        vehicles:{
+            cars: [{model: "TESLA Model 3 (TESLA-1)", quantity: 5}],
+            boats: [{model: "Hydrolift X-22 (HYDRO-1) med Evoy Galeforce Outboard (EVOY-1)", quantity:3}]
+        },
+        description: "",
+        chargers: {
+            car: 6,
+            boat: 4
+        }
+    }
+];
